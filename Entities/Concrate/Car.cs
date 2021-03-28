d0599670cc3f6a40ebf2e8762004810a30dd9a22
abdullahdemirkol01@gmail.com <abdullahdemirkol01@gmail.com>
2021-03-28 04:22:27 +0300

    C#

--- Entities/Concrate/Car.cs
+++ Entities/Concrate/Car.cs
@@ -10,7 +10,7 @@ namespace Entities.Concrate
         public int Id { get; set; }
         public int BrandId { get; set; }
         public int ColorId { get; set; }
-        public long ModelYear { get; set; }
+        public int ModelYear { get; set; }
         public decimal DailyPrice { get; set; }
         public string Description { get; set; }
     }
